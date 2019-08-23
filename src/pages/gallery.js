b38d42529a98fff014dbf197b176b38ba46d6435
George Brown <teawares@gmail.com>
2019-08-23 14:56:08 +0800

    update

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import InfiniteImages from "../components/InfiniteImages";
 
 const Gallery = () => {
   return (
@@ -11,6 +12,7 @@ const Gallery = () => {
         如今的丛林法则，就像古老而又真实的星空，是这样的，只要继续往下翻，你会看到越来越多的喵咪
         🐈🐈🐈
       </p>
+      <InfiniteImages />
     </Layout>
   );
 };

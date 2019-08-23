d96bb9a5120e3999fc42e6b355fb2f6022e81590
George Brown <teawares@gmail.com>
2019-08-23 15:15:37 +0800

    文案修改

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -9,8 +9,8 @@ const Gallery = () => {
       <SEO title="Gallery" />
       <h1 className="is-size-3">图片来自 Unsplash</h1>
       <p style={{ marginBottom: "5%" }}>
-        如今的丛林法则，就像古老而又真实的星空，是这样的，只要继续往下翻，你会看到越来越多的喵咪
-        🐈🐈🐈
+        如今的丛林法则，就像古老而又真实的星空，是这样的，越续往下翻，你会看到越多的喵咪
+        😹😹😹
       </p>
       <InfiniteImages />
     </Layout>

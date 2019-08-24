c01556bec6853b523c628fcc308318b16769f2f3
George Brown <teawares@gmail.com>
2019-08-24 09:17:03 +0800

    Update fetch.js

--- src/lambda/fetch.js
+++ src/lambda/fetch.js
@@ -5,7 +5,7 @@ exports.handler = function(event, context, callback) {
   const apiRoot = "https://api.unsplash.com";
   const accessKey = process.env.ACCESS_KEY || config.accessKey;
 
-  const catEndpoint = `${apiRoot}/photos/random?client_id=${accessKey}&count=${10}&collections='3489189,602283,8336796'`;
+  const catEndpoint = `${apiRoot}/photos/random?client_id=${accessKey}&count=${5}&collections='3489189,602283,8336796'`;
 
   axios.get(catEndpoint).then(res => {
     callback(null, {
